a12211314d57300eb023356315b3a25f556b55d9
javiOlaya <jjosaez@gmail.com>
2019-10-19 23:54:24 +0200

    create the basic structure

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
+    <title>Login&Register</title>
+</head>
+<body>
+    <h1>Bienvenido a mí aplicación</h1>
+    <h2>Inicia sesión o Regístrate</h2>
+    <div class="container">
+    <a class="b1" href="login.php">Login</a>
+    <a class="b2" href="signup.php">Sign Up</a>
+    </div>
+   
+</body>
+</html>
